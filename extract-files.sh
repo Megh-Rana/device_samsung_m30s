72629d206dbb1de5e63f76ad872fd32377450242
xyz-sundram <sundrraam@gmail.com>
2024-12-17 21:42:21 +0530

    m30s-common: patch  to use libcrypto-tm.so
    * It depends on symbol CBS_init which is removed from the latest libcrypto.

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,7 @@ function blob_fixup() {
             ;;
         vendor/lib*/libwvhidl.so)
             "${PATCHELF}" --replace-needed libprotobuf-cpp-lite-3.9.1.so libprotobuf-cpp-full-3.9.1.so "${2}"
+            "${PATCHELF}" --replace-needed libcrypto.so libcrypto-tm.so "${2}"
             ;;
         vendor/lib*/sensors.*.so)
             "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
